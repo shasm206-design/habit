44d4606e91b8d6ff4565032e7fed5cadf56a2d3e
shasm206-design <you@example.com>
2026-08-22 22:08:53 +0300

    fix: set active new api key in firebase config

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB_vYliSOjqocwmUmd1uckZsC5zpBBWwjM",
-  authDomain: "habit-1affd.firebaseapp.com",
-  projectId: "habit-1affd",
-  storageBucket: "habit-1affd.firebasestorage.app",
-  messagingSenderId: "986390418060",
-  appId: "1:986390418060:web:8fc6a6a28287673ef661a9",
-  measurementId: "G-6LF2HRJ4ES"
+  apiKey: "AIzaSyBuUZPh1vmgkPx3GeXyxixTL0GhEtFZT-M",
+  authDomain: "habit-1affd.firebaseapp.com",
+  projectId: "habit-1affd",
+  storageBucket: "habit-1affd.firebasestorage.app",
+  messagingSenderId: "986390418060",
+  appId: "1:986390418060:web:8fc6a6a28287673ef661a9",
+  measurementId: "G-6LF2HRJ4ES"
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
